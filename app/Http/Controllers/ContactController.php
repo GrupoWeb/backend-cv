7d31bedddbc5c224775543fa500512887a5bd747
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2023-01-05 21:33:11 -0600

    Primera fase de creación de usuarios del sistema -  backend

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Telefono;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Class TelefonoController
+ * Class ContactController
  * @package App\Http\Controllers
  */
-class TelefonoController extends Controller
+class ContactController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -20,47 +20,55 @@ class TelefonoController extends Controller
 
     /**
      * @OA\Get (
-     *     path="/api/telefono",
-     *     description="Get phone list",
-     *     tags={"Teléfonos"},
+     *     path="/api/contacto",
+     *     description="Get List",
+     *     tags={"Contacto"},
      *     @OA\Response(
      *          response=200,
-     *          description="Listado de Teléfonos"
+     *          description="Listado de Contactos de Emergencia"
      *      ),
      * )
      */
+
     public function index()
     {
-        $telefonos = Telefono::with(['tipoTelefono','user'])->get();
-        return response()->json($telefonos,Response::HTTP_OK);
+        $contacts = Contact::with('user')->get();
+
+        return response()->json($contacts, Response::HTTP_OK);
     }
+
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request $request
+     * @param Request $request
      * @return Response
      */
 
     /**
      * @OA\Post (
-     *     path="/api/telefono",
+     *     path="/api/contacto",
      *     description="Store a newly created resource in database",
-     *     tags={"Teléfonos"},
+     *     tags={"Contacto"},
      *     @OA\RequestBody(
      *     required = true,
      *          @OA\MediaType(
      *              mediaType="application/json",
      *              @OA\Schema (
      *                  @OA\Property(
+     *                      property = "contact_name",
+     *                      description = "Nombre del Contacto",
+     *                      type = "string",
+     *                  ),
+     *                  @OA\Property(
      *                      property = "number",
      *                      description = "Número de Teléfono",
      *                      type = "integer",
      *                  ),
      *                  @OA\Property(
-     *                      property = "tipo_telefono_id",
-     *                      description = "Identificación del tipo de teléfono",
-     *                      type = "integer",
+     *                      property = "relationship",
+     *                      description = "Parentesco",
+     *                      type = "string",
      *                  ),
      *                  @OA\Property(
      *                      property = "user_id",
@@ -71,18 +79,18 @@ class TelefonoController extends Controller
      *          ),
      *     ),
      *     @OA\Response(
-     *      response=201,
-     *      description="Crear un Teléfono",
+     *      response=202,
+     *      description="Crear un contacto",
      *     )
      * )
      */
     public function store(Request $request)
     {
-        request()->validate(Telefono::$rules);
+        request()->validate(Contact::$rules);
 
-        $telefono = Telefono::create($request->all());
+        $contact = Contact::create($request->all());
 
-        return response()->json($telefono, Response::HTTP_OK);
+        return response()->json($contact, Response::HTTP_OK);
     }
 
     /**
@@ -94,26 +102,26 @@ class TelefonoController extends Controller
 
     /**
      * @OA\Get (
-     *     path="/api/telefono/{id}",
-     *     description="Get a record of phone type",
-     *     tags={"Teléfonos"},
+     *     path="/api/contacto/{id}",
+     *     description="Get a record of contact",
+     *     tags={"Contacto"},
      *     @OA\Parameter (
      *      name="id",
      *      in="path",
-     *      description="id for Phone",
+     *      description="id for contact",
      *      required=true,
      *      ),
      *     @OA\Response(
      *          response=200,
-     *          description="Obtener un tipo de teléfono",
+     *          description="Obtener un contacto",
      *      )
      * )
      */
     public function show(int $id)
     {
-        $telefono = Telefono::find($id);
+        $contact = Contact::find($id);
 
-        return response()->json($telefono, Response::HTTP_OK);
+        return response()->json($contact, Response::HTTP_OK);
     }
 
 
@@ -124,42 +132,42 @@ class TelefonoController extends Controller
 
     /**
      * @OA\Get (
-     *     path="/api/telefono/usuario/{id}",
-     *     description="Get telefono information by ID",
-     *     tags={"Teléfonos"},
+     *     path="/api/contacto/usuario/{id}",
+     *     description="Get contact by ID",
+     *     tags={"Contacto"},
      *     @OA\Parameter (
      *      name="id",
      *      in="path",
-     *      description="id",
+     *      description="user_id",
      *      required=true,
      *      ),
      *     @OA\Response(
      *          response=200,
-     *          description="Obtener la información de los teléfonos por su id",
+     *          description="Obtener la información de un contacto por su id",
      *      )
      * )
      */
 
     public function showByUserId(int $id){
-        $telefono = Telefono::selectRaw('telefonos.id, telefonos.number, tipo_telefonos.description')->join('tipo_telefonos','tipo_telefonos.id','=','telefonos.tipo_telefono_id')
-            ->where(['telefonos.user_id'    =>  $id])->get();
-        return response()->json($telefono, Response::HTTP_OK);
+        $contact = Contact::selectRaw('id, contact_name, number, relationship')
+            ->where(['user_id'    =>  $id])->get();
+        return response()->json($contact, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param Telefono $telefono
+     * @param Contact $contact
      * @param $id
      * @return Response
      */
 
     /**
      * @OA\Put (
-     *     path="/api/telefono/{id}",
-     *     description="Update a Phone",
-     *     tags={"Teléfonos"},
+     *     path="/api/contacto/{id}",
+     *     description="Update a contact",
+     *     tags={"Contacto"},
      *     @OA\Parameter (
      *          name="id",
      *          in="path",
@@ -172,34 +180,39 @@ class TelefonoController extends Controller
      *              mediaType = "application/json",
      *              @OA\Schema (
      *                  @OA\Property(
+     *                      property = "contact_name",
+     *                      description = "nombre del contacto",
+     *                      type = "string",
+     *                  ),
+     *                  @OA\Property(
      *                      property = "number",
-     *                      description = "number",
+     *                      description = "número de teléfono de contacto",
      *                      type = "integer",
      *                  ),
      *                  @OA\Property(
-     *                      property = "tipo_telefono_id",
-     *                      description = "Phone type identification",
-     *                      type = "integer",
+     *                      property = "relationship",
+     *                      description = "Parentesco",
+     *                      type = "string",
      *                  ),
      *                  @OA\Property(
      *                      property = "user_id",
-     *                      description = "User Identification",
+     *                      description = "Identificación del usuario",
      *                      type = "integer",
      *                  ),
      *              ),
      *          ),
      *     ),
      *     @OA\Response(
-     *     response=201,
-     *     description="Actualizar un Teléfono",
+     *     response=200,
+     *     description="Actualizar un contacto",
      * ),
      * )
      */
-    public function update(Request $request, Telefono $telefono, $id)
+    public function update(Request $request, Contact $contact, $id)
     {
-        request()->validate(Telefono::$rules);
+        request()->validate(Contact::$rules);
 
-        $telefono->find($id)->update($request->all());
+        $contact->find($id)->update($request->all());
 
         return response()->json('Actualizado correctamente',Response::HTTP_OK);
     }
@@ -211,25 +224,25 @@ class TelefonoController extends Controller
 
     /**
      * @OA\Delete (
-     *     path="/api/telefono/{id}",
-     *     description="Delete a single phone on the ID supplied",
-     *     tags={"Teléfonos"},
+     *     path="/api/contacto/{id}",
+     *     description="Delete a single contact on the ID supplied",
+     *     tags={"Contacto"},
      *     @OA\Parameter (
-     *      description = "ID of Phone to delete",
+     *      description = "ID of contact to delete",
      *      in = "path",
      *      name = "id",
      *      required = true,
      *     ),
      *     @OA\Response(
      *     response=204,
-     *     description="Eliminar un telefono",
+     *     description="Eliminar un contacto",
      * )
      * )
      */
     public function destroy(int $id)
     {
-        $telefono = Telefono::find($id)->delete();
+        $contact = Contact::find($id)->delete();
 
-        return response()->json($telefono, Response::HTTP_OK);
+        return response()->json($contact, Response::HTTP_NO_CONTENT);
     }
 }
